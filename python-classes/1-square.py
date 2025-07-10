e026d936361124fa07a9c68468e18a0521ed5b00
MBeckyAlek <a.alek@alustudent.com>
2025-07-10 15:13:51 +0200

    wueh

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-"""Defines a class square."""
+
+"""Define a class Square."""
 
 
 class Square:
     """Represent a square."""
 
     def __init__(self, size):
-
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
         self.__size = size
-        
