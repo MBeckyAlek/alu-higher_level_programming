9e8c1e4008e8a42064f91f1322b080e44fa0e7b5
MBeckyAlek <a.alek@alustudent.com>
2025-07-10 14:56:34 +0200

    yoh

---
+++ python-exceptions/python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+  2 """Square class to represent a square"""
+  3
+  4
+  5 class Square:
+  6     """
+  7     Defines a Square and its basic properties
+  8     """
+  9     pass
